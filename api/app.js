2662c09a20174be550c7b552f8b7852ff5f442af
Divyansh Srivastava <divyanshsrivastava20@gmail.com>
2024-12-04 13:58:56 +0530

    Restructured the directory structure.
    - Add:  Restructured the directory structure.
    
    - Add: vercel.json
    
    - Change: server directory to api.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // *Routes
 const authRoute = require("./routes/auth.js"); //? authentication route
@@ -33,7 +34,9 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html")
+    );
   });
 }
 
